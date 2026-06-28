b3df90df770c2d8ce506af8703d5e07ec57e8021
adithyakanake-blip <adithyakanake@gmail.com>
2026-06-28 18:26:19 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,10 @@
+name = input("what is your name? ")
+age = input("what is your age? ")
+branch = input("your branch? ")
+
+print(f"\n---student profile---")
+print(f"Name :{name}")
+print(f"Age :{age}")
+print(f"Branch :{branch}")
+print(f"\nHello{name}!your ai/ml journey starts today")
+
