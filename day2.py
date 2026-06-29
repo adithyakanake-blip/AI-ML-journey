3aca1c7b523b49d42e120555dc81522486310705
adithyakanake-blip <adithyakanake@gmail.com>
2026-06-29 19:35:41 +0530

    Add files via upload
    completed my day2 learning python

---
+++ day2.py
@@ -0,0 +1,19 @@
+#variables & data types
+name = input("what is your name? ") #name:adithya
+age = int(input("what is your age? "))
+branch = input("your branch? ")
+gpa = float(input("1st sem gpa:"))
+
+print("\n---my profile---")
+print("Name :{name}")
+print("Age :{age}")
+print("Branch :{branch}")
+print("Gpa:{gpa}")
+
+print(type(name))
+print(type(age))
+print(type(branch))
+print(type(gpa))
+
+
+
