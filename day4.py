05fc78d923627767f71683df0bed0f30b3ffa0f6
adithyakanake-blip <adithyakanake@gmail.com>
2026-07-04 20:15:21 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,28 @@
+# conditiional expresssion
+
+#grade calculator
+
+score =int(input("Enter the score:"))
+
+if (score >= 90):
+    print("grade:A")
+
+elif(score>=80):
+    print("grade:B")
+
+elif(score>=70):
+    print("grade:C")
+
+elif(score>=60):
+    print("grade:D")
+
+elif(score>=50):
+    print("grade:E")
+
+elif(score>=40):
+    print("grade:F")
+
+else:
+    print("student is failed")
+
+
