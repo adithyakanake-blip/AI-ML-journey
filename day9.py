edbc522923d759ed633f7f5814378333a438ec63
Adithya <adithyakanake@gmail.com>
2026-07-07 23:46:30 +0530

    Update day9.py

--- day9.py
+++ day9.py
@@ -10,10 +10,26 @@ class student:
         print(f"Age:{self.age}")
         print(f"Branch:{self.branch}")
         print(f"Gpa:{self.gpa}")
+def study(self):
+        print(f"{self.name} is studying in {self.branch}")
+
+    def get_grade(self):
+        if (self.gpa >=9.0):
+            print(f"{self.name}'s Grade:A+")
+        elif (self.gpa >=8.0):
+            print(f"{self.name}'s Grade:A")
+        elif (self.gpa >=7.0):
+            print(f"{self.name}'s Grade:B")
+
 student1 =student("kiyo",20,"csm",8.5)
 student2 =student("kiyomi",21,"cse",8.9)
 student1.introduce()
-student2.introduce()
+student1.study()
+student1.get_grade()
 
+student2.introduce()
+student2.study()
+
+        
 
         
